0ad39d71736ee74a80a102ae3ac4f6db90c9354d
Zach Murphy <murphyz@onid.orst.edu>
2014-11-20 09:13:04 -0800

    cleaned up the code a little

--- lab2.c
+++ lab2.c
@@ -37,7 +37,9 @@ void DebounceSwitch();
 
 uint16_t bcd;
 
-//holds data for selecing different digits in the SSD
+/*****************************************************************************
+** holds data for selecing different digits in the SSD
+*****************************************************************************/
 uint8_t digit_data[5] = {
 	0b00000000,//0
 	0b00010000,//1
@@ -47,7 +49,9 @@ uint8_t digit_data[5] = {
 };
 
 
-//holds data to be sent to the segments. logic zero turns segment on
+/*****************************************************************************
+**holds data to be sent to the segments. logic zero turns segment on
+*****************************************************************************/
 uint8_t segment_data[5] = { 
 	0b11111111,
 	0b11111111,
@@ -56,7 +60,9 @@ uint8_t segment_data[5] = {
 	0b11111111
 };
 
-//decimal to 7-segment LED display encodings, logic "0" turns on segment
+/*****************************************************************************
+decimal to 7-segment LED display encodings, logic "0" turns on segment
+*****************************************************************************/
 uint8_t dec_to_7seg[12] = {
 	0b11000000, //0
 	0b11111001, //1
@@ -71,14 +77,17 @@ uint8_t dec_to_7seg[12] = {
 	0b11111111, //1
 	0b10011000 //9
 }; 
-//initialize SPI mode
+/*****************************************************************************
+initialize SPI mode
+*****************************************************************************/
 void spi_init(void){
 	DDRB  |=   0xff;          //Turn on SS, MOSI, SCLK
 	SPCR  |=   (1 << SPE) | (1 << MSTR);     //set up SPI mode
 	SPSR  |=   (1 << SPI2X);           // double speed operation
 }//spi_init
-
-//read values from the encoder and increment count as appropriate
+/*****************************************************************************
+** Data is tranlsated from the knob's current position relative to it's last position
+*****************************************************************************/
 void read_enc(knob){
 
 	static uint8_t enc_state1=0, enc_state2=0, enc_state;
@@ -119,8 +128,10 @@ void read_enc(knob){
 }
 
 
-//interupt service routine for timercounter0. Used to scan for button presses and data from the ecoder. I have added functions create a delay between presing a button
-//and having the controller take action to prevent action from happening when trying to press two buttons at once.  
+/*****************************************************************************
+interupt service routine for timercounter0. Used to scan for button presses and data from the ecoder. I have added functions create a delay between presing a button
+and having the controller take action to prevent action from happening when trying to press two buttons at once.  
+*****************************************************************************/
 ISR(TIMER0_OVF_vect){
 	static uint16_t count_7ms = 0;
 	static uint8_t  button_delay=0, button_release_delay=0;       	//delay counters to create a gap between pressing or releaseing a button and 
@@ -168,10 +179,16 @@ ISR(TIMER0_OVF_vect){
 	read_enc(KNOB1);				//interpreate encoder data and increment/decrement count
 	read_enc(KNOB2);				//interpreate encoder data and increment/decrement count
 }
+/*****************************************************************************
+ISR for timer/counter2: set up in fast PWM mode and toggles PE1 to create the alarm signal 
+*****************************************************************************/
 ISR(TIMER2_COMP_vect){
 	PORTE ^= (1 << 0);
 }
 
+/*****************************************************************************
+ISR for the ADC. Whenever the adc signals a completion it stores the upper 8 bits
+*****************************************************************************/
 ISR(ADC_vect){
 
 
@@ -180,17 +197,25 @@ ISR(ADC_vect){
 
 }
 
-//initialize timercounter0
+/*****************************************************************************
+initialize timer/counter0 to run in normal mode. Scans buttons and enconders and controls the clock display value 
+*****************************************************************************/
 void int0_init(){
 	TIMSK |= (1 << TCNT0);			//enable interrupts
 	TCCR0 |= (1 << CS02) | (1 << CS00); 	//normal mode, prescale by 128
 }
 
+/*****************************************************************************
+initialize timer/counter2 to normal mode. Creates the alarm audio signal
+*****************************************************************************/
 void int2_init(){
 	TIMSK |= (1 << OCIE2);			//enable interrupts:
 	TCCR2 |= (1 << WGM21) | (1 << WGM20) | (1 << COM21) | (1 << CS21); 	//CTC mode, prescale by 128
 }
 
+/*****************************************************************************
+initialize timer/counter3 to fast pwm mode. Create a duty cycle to control speaker volume
+*****************************************************************************/
 void int3_init(){
 
 	TCCR3A |= (1<<COM3A1) | (1<<COM3A0) | (1 << WGM30);//fast pwm, set on match, clear@bottom, 
@@ -201,7 +226,9 @@ void int3_init(){
 	OCR3A = 0x00a0;
 	
 }
-//debouncing switch checks for 12 consecutive signals from same button before returning 1
+/*****************************************************************************
+debouncing switch checks for 12 consecutive signals from same button before returning 1
+*****************************************************************************/
 void DebounceSwitch(){
 	uint8_t i,j;
 	state[check_index++%MAX_CHECKS]=0xff - PINA;
@@ -211,7 +238,9 @@ void DebounceSwitch(){
 }
 
 
-//returns the display to segmentn_data in decimal
+/*****************************************************************************
+returns the display to segmentn_data in decimal
+*****************************************************************************/
 void segsum(uint16_t sum) {
 	int num_d=0, i;
 	int temp, temp2;
@@ -241,6 +270,9 @@ void segsum(uint16_t sum) {
 
 }
 
+/*****************************************************************************
+initialize the ADC: freerunning mode, clock 1024 prescale
+*****************************************************************************/
 void adc_init(){
 	PORTF |= (1 << 2);
 	DDRF &= ~(1 << 2);
@@ -253,6 +285,9 @@ void adc_init(){
 
 }
 
+/*****************************************************************************
+main
+*****************************************************************************/
 uint8_t main()
 {
 	int i, temp, delay;
