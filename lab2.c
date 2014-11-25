10f0e92fd60e0ca925f6b081f91d9744745675b1
Zach <smurph32@hotmail.com>
2014-11-24 22:08:07 -0800

    working on  timer overflow in reverse

--- lab2.c
+++ lab2.c
@@ -1,127 +1,130 @@
-//Zachary Murphy
-//11.5.14
+	//Zachary Murphy
+	//11.5.14
 
 
-//  HARDWARE SETUP:
-//  PORTA is connected to the segments of the LED display. and to the pushbuttons.
-//  PORTA.0 corresponds to segment a, PORTA.1 corresponds to segement b, etc.
-//  PORTB bits 4-6 go to a,b,c inputs of the 74HC138.
-//  PORTB bit 7 goes to the PWM transistor base.
+	//  HARDWARE SETUP:
+	//  PORTA is connected to the segments of the LED display. and to the pushbuttons.
+	//  PORTA.0 corresponds to segment a, PORTA.1 corresponds to segement b, etc.
+	//  PORTB bits 4-6 go to a,b,c inputs of the 74HC138.
+	//  PORTB bit 7 goes to the PWM transistor base.
 
-#define F_CPU 16000000 // cpu speed in hertz 
-#define TRUE 1
-#define FALSE 0
-#define MAX_CHECKS 12
-#include <avr/io.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
+	#define F_CPU 16000000 // cpu speed in hertz 
+	#define TRUE 1
+	#define FALSE 0
+	#define MAX_CHECKS 12
+	#include <avr/io.h>
+	#include <util/delay.h>
+	#include <avr/interrupt.h>
 
-#define KNOB1 4
-#define KNOB2 1
+	#define KNOB1 4
+	#define KNOB2 1
 
-#define DECIMAL 0
-#define TWELVEHOUR  1
-#define TWENTYFOURHOUR  2
+	#define DECIMAL 0
+	#define TWELVEHOUR  1
+	#define TWENTYFOURHOUR  2
 
-#define Y7 0b01110000
-#define DC 0b10001111
+	#define Y7 0b01110000
+	#define DC 0b10001111
 
-uint8_t debounced_state = 0; // Debounced state of the switches
-uint8_t state[MAX_CHECKS]; // Array that maintains bounce status
-uint8_t check_index, mode = 0x00; //holds count for display 0; // Pointer into State
-static uint8_t enc=0; 
-int show_alarm=0, lock = 0, press = 0, button=0, count_add=1;
+	#define MODCAP 1440
+	uint8_t debounced_state = 0; // Debounced state of the switches
+	uint8_t state[MAX_CHECKS]; // Array that maintains bounce status
+	uint8_t check_index, mode = 0x00; //holds count for display 0; // Pointer into State
+	static uint8_t enc=0; 
+	int show_alarm=0, lock = 0, press = 0, button=0, count_add=1;
 
-uint32_t count=0, alarm=0;
+	uint32_t count=0, alarm=0;
 
-void DebounceSwitch();
-
-
-uint16_t bcd;
-
-//holds data for selecing different digits in the SSD
-uint8_t digit_data[5] = {
-	0b00000000,//0
-	0b00010000,//1
-	0b00100000,//:
-	0b00110000,//2
-	0b01000000 //3
-};
+	void DebounceSwitch();
 
 
-//holds data to be sent to the segments. logic zero turns segment on
-uint8_t segment_data[5] = { 
-	0b11111111,
-	0b11111111,
-	0b11111111,
-	0b11111111,
-	0b11111111
-};
+	uint16_t bcd;
 
-//decimal to 7-segment LED display encodings, logic "0" turns on segment
-uint8_t dec_to_7seg[12] = {
-	0b11000000, //0
-	0b11111001, //1
-	0b10100100, //2
-	0b10110000, //3
-	0b10011001, //4
-	0b10010010, //5
-	0b10000010, //6
-	0b11111000, //7
-	0b10000000, //8
-	0b10011000, //9
-	0b11111111, //1
-	0b10011000 //9
-}; 
-//initialize SPI mode
-void spi_init(void){
-	DDRB  |=   0xff;          //Turn on SS, MOSI, SCLK
-	SPCR  |=   (1 << SPE) | (1 << MSTR);     //set up SPI mode
-	SPSR  |=   (1 << SPI2X);           // double speed operation
-}//spi_init
+	//holds data for selecing different digits in the SSD
+	uint8_t digit_data[5] = {
+		0b00000000,//0
+		0b00010000,//1
+		0b00100000,//:
+		0b00110000,//2
+		0b01000000 //3
+	};
 
-//read values from the encoder and increment count as appropriate
-void read_enc(knob){
 
-	static uint8_t enc_state1=0, enc_state2=0, enc_state;
-	uint8_t temp = (enc/knob)%4, *target;//isolates 2 digits relevant to encoder
+	//holds data to be sent to the segments. logic zero turns segment on
+	uint8_t segment_data[5] = { 
+		0b11111111,
+		0b11111111,
+		0b11111111,
+		0b11111111,
+		0b11111111
+	};
 
-	if(show_alarm) 	target = &alarm;
-	else		target = &count;	
+	//decimal to 7-segment LED display encodings, logic "0" turns on segment
+	uint8_t dec_to_7seg[12] = {
+		0b11000000, //0
+		0b11111001, //1
+		0b10100100, //2
+		0b10110000, //3
+		0b10011001, //4
+		0b10010010, //5
+		0b10000010, //6
+		0b11111000, //7
+		0b10000000, //8
+		0b10011000, //9
+		0b11111111, //1
+		0b10011000 //9
+	}; 
+	//initialize SPI mode
+	void spi_init(void){
+		DDRB  |=   0xff;          //Turn on SS, MOSI, SCLK
+		SPCR  |=   (1 << SPE) | (1 << MSTR);     //set up SPI mode
+		SPSR  |=   (1 << SPI2X);           // double speed operation
+	}//spi_init
 
-	if(knob == KNOB1) enc_state = enc_state1;
-	if(knob == KNOB2) enc_state = enc_state2;
+	//read values from the encoder and increment count as appropriate
+	void read_enc(knob){
 
-	enc_state=enc_state%4;
+		static uint8_t enc_state1=0, enc_state2=0, enc_state;
+		uint8_t temp = (enc/knob)%4;
+		uint32_t *target;//isolates 2 digits relevant to encoder
 
-	switch(enc_state){
-		case 0://neutral position
-			if (temp == 1) enc_state++; //indicating we clicked encoder right from neutral position
-			if (temp == 2) enc_state --;//indicating we clicked encoder left from neutral position
-			break;
-		case 1://one click right, three clicks left
-			if(temp == 0) enc_state++;
-			if(temp == 3){
-				enc_state++;
-				*target = (*target - count_add)%3600;			
-			}
-			break;
-		case 2://two clicks right, two click left
-			if(temp == 2) enc_state++;
-			if(temp == 1) enc_state--;
-			break;
-		case 3://three clicks right, one click left
-			if(temp == 3){
-				enc_state++;
-				*target = (*target + count_add)%3600;			
-			}
-			if(temp == 0) enc_state--;
-			break;
+		if(show_alarm) 	target = &alarm;
+		else		target = &count;	
 
+		if(knob == KNOB1) enc_state = enc_state1;
+		if(knob == KNOB2) enc_state = enc_state2;
+
+		enc_state=enc_state%4;
+
+		switch(enc_state){
+			case 0://neutral position
+				if (temp == 1) enc_state++; //indicating we clicked encoder right from neutral position
+				if (temp == 2) enc_state --;//indicating we clicked encoder left from neutral position
+				break;
+			case 1://one click right, three clicks left
+				if(temp == 0) enc_state++;
+				if(temp == 3){
+					enc_state++;
+					*target = (*target - count_add);
+					if(*target<0) *target = MODCAP+*target;			
+				}
+				break;
+			case 2://two clicks right, two click left
+				if(temp == 2) enc_state++;
+				if(temp == 1) enc_state--;
+				break;
+			case 3://three clicks right, one click left
+				if(temp == 3){
+					enc_state++;
+					*target = (*target + count_add)%MODCAP;			
+				}
+				if(temp == 0) enc_state--;
+				break;
+
+		}
+		if(knob == KNOB1) enc_state1 = enc_state;
+		if(knob == KNOB2) enc_state2 = enc_state;
 	}
-	if(knob == KNOB1) enc_state1 = enc_state;
-	if(knob == KNOB2) enc_state2 = enc_state;
-}
 
 
 //interupt service routine for timercounter0. Used to scan for button presses and data from the ecoder. I have added functions create a delay between presing a button
@@ -155,8 +158,8 @@ ISR(TIMER0_OVF_vect){
 
 
 	//This increments count approximately every 1 second. 
-	if ((count_7ms % 512)==0) //?? interrupts equals one half second 
-		count = (count + 1);//bound the count to 0 - 1023
+//	if ((count_7ms % 512)==0) //?? interrupts equals one half second 
+//		count = ((count + 1)%MODCAP);//bound the count to 0 - 1023
 
 
 	SPDR = mode;//display current mode on bar graph
@@ -241,8 +244,9 @@ void DebounceSwitch(){
 
 
 //returns the display to segmentn_data in decimal
-void segsum(uint16_t sum, int c_mode) {
-	int num_d=0, i, hour, min;
+void segsum(uint32_t sum, int c_mode) {
+	int num_d=0, i;
+	long hour, min;
 	int temp, temp2, am;
 	temp = sum;
 	while(sum >= (pow(10, num_d))) {num_d++;}//record number of digits
@@ -271,7 +275,7 @@ void segsum(uint16_t sum, int c_mode) {
 			for(i=5;i>num_d;i--) {segment_data[i-1] = 0b11111111;}	
 			break;
 		case TWELVEHOUR:
-			hour = sum/60%24;
+			hour = (sum/60)%24;
 			if(hour > 11) am = 0;
 			else am = 1;
 			hour = hour%12;
@@ -287,7 +291,7 @@ void segsum(uint16_t sum, int c_mode) {
 			break;
 
 		case TWENTYFOURHOUR:
-			hour = sum/60%24;
+			hour = (sum/60)%24;
 			min = (sum)%60;
 
 
@@ -346,6 +350,13 @@ uint8_t main()
 				case 2:
 					mode ^= 0x40;
 					break;
+				case 8 :
+					mode ^= 0x01;
+					sei();
+					while(1){
+						PORTB = (count/256);
+					}
+					break;
 				case 4:
 					mode ^= 0x20;
 					break;
@@ -372,14 +383,17 @@ uint8_t main()
 		if(show_alarm) 	segsum(alarm, TWENTYFOURHOUR);
 		else 		segsum(count, TWENTYFOURHOUR);
 
+		//		sei();         //enable interrupts before entering loop
 		for(i=4;i>=0;i--){//bound a counter (0-4) to keep track of digit to display 
 			DDRA = 0xff;//make PORTA an output
 			PORTA = segment_data[i];//segment_data[i];
 			PORTB &= DC;
 			PORTB |= digit_data[i];//update digit to display
-			_delay_us(400);
+			_delay_us(300);
 			PORTA = 0xff;//isegment_data[i];
+			_delay_us(400);
 		}
+		//		sei();         //enable interrupts before entering loop
 	}
 
 	return 0;
